15a6d4c039904eb7c6217426a71a01c3073245fe
artoftadashi <samuelolanrewaju05@gmail.com>
2024-11-26 14:16:13 +0000

    added marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,51 +1,37 @@
-export default function Marquee() {
-  return (
-    <div className="relative flex items-center overflow-x-hidden bg-[#06AC76] text-[#01121A] border-t-[3px] border-b-[3px] border-[#01121A] text-lg">
-      <div className="py-4 animate-marquee whitespace-nowrap font-geist font-bold uppercase flex items-center ">
-        <span className=" mx-4  ">
-          <img src="/images/heart.svg" />
-        </span>
-        <span className=" mx-4  ">
-          <span className="text-[#01121A] mx-4">•</span> {"</"}Let's Work
-          Together!{">"}
-          <span className="text-[#01121A] mx-4">•</span>
-        </span>
-        <span className="  mx-4 ">
-          <img src="/images/shine.svg" />
-        </span>
-        <span className=" mx-4  ">
-          <span className="text-[#01121A] mx-4">•</span>
-          {"</"}Let's Work Together!{">"}
-          <span className="text-[#01121A] mx-4">•</span>
-        </span>
-        <span className="">
-          <img src="/images/rainbow.svg" />
-        </span>
-      </div>
+import React from "react";
 
-      <div className="absolute top-0 py-4 animate-marquee2 whitespace-nowrap font-geist font-bold uppercase flex items-center">
-        <span className=" mx-4  ">
-          <span className="text-[#01121A] mx-4">•</span>
-          {"</"}Let's Work Together!{">"}
-          <span className="text-[#01121A] mx-4">•</span>
-        </span>
-        <span className="mx-4  ">
-          <img src="/images/Music.svg" />
-        </span>
-        <span className="mx-4  ">
-          <span className="text-[#01121A] mx-4">•</span>
-          {"</"}Let's Work Together!{">"}
-          <span className="text-[#01121A] mx-4">•</span>
-        </span>
-        <span className=" mx-4 ">
-          <img src="/images/camera.svg" />
-        </span>
-        <span className="">
-          <span className="text-[#01121A] mx-4">•</span>
-          {"</"}Let's Work Together!{">"}
-          <span className="text-[#01121A] mx-4">•</span>
-        </span>
+const Marquee = () => {
+  const content = [
+    { id: 1, image: "/images/heart.svg", text: "•  come with your babe  •" },
+    { id: 2, image: "/images/shine.svg", text: "•  come with your bff  •" },
+    { id: 3, image: "/images/rainbow.svg", text: "•  come with your bro  •" },
+    { id: 4, image: "/images/Music.svg", text: "•  come with your boo  •" },
+    {
+      id: 5,
+      image: "/images/camera.svg",
+      text: "• come with your sneaky link •",
+    },
+    {
+      id: 6,
+      image: "/images/lightening.svg",
+      text: "• come with your crush •",
+    },
+  ];
+
+  return (
+    <div className="overflow-hidden bg-[#06AC76] py-4 border-y-[3px] border-[#01121A]">
+      <div className="flex animate-marquee whitespace-nowrap gap-8">
+        {content.map((item) => (
+          <div key={item.id} className="flex items-center gap-8 px-4">
+            <img src={item.image} alt="Image" className="h-8 w-8" />
+            <p className="text-xl font-geist font-semibold uppercase">
+              {item.text}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default Marquee;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,16 @@ export default {
         geist: ["Geist", "sans-serif"],
       },
       animation: {
-        marquee: "marquee 25s linear infinite",
-        marquee2: "marquee2 25s linear infinite",
+        marquee: "marquee 10s linear infinite alternate",
       },
       keyframes: {
         marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        marquee2: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(0%)" },
+          "0%": {
+            transform: "translateX(100%)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
         },
       },
     },

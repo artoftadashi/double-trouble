1749af801fdf2f9e83bc64e353a0339f105a39af
artoftadashi <samuelolanrewaju05@gmail.com>
2024-12-06 00:58:53 +0000

    added responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,49 @@
+import { useState, useEffect } from "react";
+
 export default function Header() {
   const elipse = "w-[6px] h-[6px] rounded-full bg-[#01121A]";
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    setIsClosing(false);
+  };
+  const handleClose = () => {
+    setIsClosing(true); // Start the reverse animation
+    setTimeout(() => {
+      setIsOpen(false); // Close the menu after the animation
+      setIsClosing(false); // Reset the closing state
+    }, 500); // Match this to the duration of your reverse animation
+  };
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-y-hidden");
+    } else {
+      document.body.classList.remove("overflow-y-hidden");
+    }
+    return () => {
+      document.body.classList.remove("overflow-y-hidden");
+    };
+  }, [isOpen]);
+
   return (
-    <div className="flex justify-between px-12 py-12 uppercase font-geist ">
+    <div className="relative flex justify-between px-6 py-12 lg:p-12 uppercase font-geist ">
       <div className="w-fit relative flex justify-center">
-        <div className="bg-[#FFFAEC] flex justify-center items-center rounded-md px-4 py-2 border-[3px] border-[#01121A] gap-3 shadow-md ">
+        <div className="bg-[#FFFAEC] w-fit flex justify-center items-center rounded-md px-4 py-2 border-[3px] border-[#01121A] gap-3 shadow-md ">
           <div className={elipse}></div>
-          <p className="font-semibold text-lg">11th november, 2024</p>
+          <p className="font-semibold text-[.8rem] lg:text-lg">
+            11th november, 2024
+          </p>
           <div className={elipse}></div>
         </div>
         <img
           src="/images/hanger.svg"
-          className="absolute w-[80px] h-[35px] top-[-32px]"
+          className="absolute w-[50px] lg:w-[80px] h-[35px] top-[-28px] lg:top-[-32px]"
         />
       </div>
-      <ul className="flex gap-10 items-center uppercase font-semibold text-lg">
+      <ul className="lg:flex gap-10 items-center uppercase font-semibold text-lg hidden ">
         <li>
           <a href="#">about</a>
         </li>
@@ -24,6 +54,38 @@ export default function Header() {
           <a href="#">faqs</a>
         </li>
       </ul>
+
+      {/* drop down menu button */}
+
+      <button
+        onClick={handleOpen}
+        className="w-[3rem] h-[3rem] border-[3px] border-[#01121A] rounded-full lg:hidden flex flex-col gap-1 justify-center items-center bg-[#FFFAEC] z-[100]"
+      >
+        <div className="w-5 h-[.2rem] bg-[#01121A] rounded-3xl"></div>
+        <div className="w-5 h-[.2rem] bg-[#01121A] rounded-3xl"></div>
+      </button>
+      {isOpen && (
+        <div className="animate-fade-down animate-duration-500 absolute top-0 left-0 h-[100vh] w-[100vw] lg:hidden bg-[#FFFAEC] backdrop-blur-md z-[100] flex flex-col justify-center items-center gap-20 overflow-y-hidden  ">
+          <ul className="text-center text-4xl font-geist flex flex-col gap-5">
+            <li className="animate-fade-right animate-delay-500">
+              <a href="#">about</a>
+            </li>
+            <li className="animate-fade-right animate-delay-[600ms]">
+              <a href="#">activities</a>
+            </li>
+            <li className="animate-fade-right animate-delay-[700ms]">
+              <a href="#">faqs</a>
+            </li>
+          </ul>
+          <button
+            onClick={handleClose}
+            className="w-[3rem] h-[3rem] border-[3px] border-[#01121A] rounded-full lg:hidden flex flex-col gap-1 justify-center items-center bg-[#FFFAEC] z-[100]"
+          >
+            <div className="absolute rotate-45 w-5 h-[.2rem] bg-[#01121A] rounded-3xl"></div>
+            <div className="absolute -rotate-45 w-5 h-[.2rem] bg-[#01121A] rounded-3xl"></div>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import Button from "./Button";
 export default function Hero() {
   return (
     <section className=" pb-32 pt-8 relative">
-      <div className="flex flex-col justify-center items-center gap-8 z-20">
+      <div className="flex flex-col justify-center items-center gap-12 lg:gap-8 z-20">
         <img
           alt="zikoko x smirnoff"
           src="/images/zikoko-x-smirmoff.svg"
-          className="z-20"
+          className="z-20 w-[50%] lg:w-[60%]"
         />
         <img
           alt="double trouble"
           src="/images/logo.png"
-          className="z-20 w-[60vw]"
+          className="z-20 w-[90vw] lg:w-[60vw]"
         />
-        <div className="flex gap-4 pt-16">
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-4 lg:pt-16">
           <Button text="buy your ticket" />
           <Button text="become a sponsor" />
         </div>
@@ -23,33 +23,33 @@ export default function Hero() {
       <img
         alt="event images"
         src="images/hero-img1.png"
-        className="absolute top-[4rem] left-[-1rem] z-[5] "
+        className="w-[30vw] absolute top-[-3rem] lg:top-[4rem] left-[-2rem] z-50 lg:z-[5] "
       />
       <img
         alt="event images"
         src="images/hero-img2.png"
-        className="absolute bottom-[4rem] right-[3rem] z-[5]"
+        className="w-[30vw] absolute bottom-[1rem] right-[-2rem] lg:bottom-[4rem] lg:right-[3rem] z-[5]"
       />
       {/* cloud images */}
       <img
         alt="cloud"
         src="/images/cloud.svg"
-        className="absolute left-[6rem] top-[-5rem] z-10"
+        className=" w-[80vw] lg:w-[45vw] absolute left-[-4rem] top-[-1rem] lg:left-[6rem] lg:top-[-5rem] z-10"
       />
       <img
         alt="cloud"
         src="/images/cloud.svg"
-        className="absolute right-[-10rem] top-[6rem]"
+        className=" w-[80vw] lg:w-[45vw] absolute right-[-10rem] top-[11rem] lg:right-[-10rem] lg:top-[6rem]"
       />
       <img
         alt="cloud"
         src="/images/cloud.svg"
-        className="absolute w-[762px] bottom-[7rem] left-[-7rem]"
+        className=" w-[80vw] lg:w-[45vw] absolute bottom-[4rem] left-[-4rem] lg:bottom-[7rem] lg:left-[-7rem]"
       />
       <img
         alt="cloud"
         src="/images/cloud.svg"
-        className="absolute bottom-[3rem] right-[-28rem]"
+        className=" w-[80vw] lg:w-[45vw] absolute bottom-0 right-[-14rem] lg:bottom-[3rem] lg:right-[-28rem]"
       />
     </section>
   );

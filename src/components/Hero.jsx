73b736a3ac3f258f644bf5a85e23c1bdb2e814f7
artoftadashi <samuelolanrewaju05@gmail.com>
2024-12-06 01:34:02 +0000

    bug fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ export default function Hero() {
         <img
           alt="zikoko x smirnoff"
           src="/images/zikoko-x-smirmoff.svg"
-          className="z-20 w-[50%] lg:w-[60%]"
+          className="z-20 w-[50%] lg:w-[35%]"
         />
         <img
           alt="double trouble"
           src="/images/logo.png"
-          className="z-20 w-[90vw] lg:w-[60vw]"
+          className="z-20 w-[90vw] lg:w-[50vw]"
         />
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-4 lg:pt-16">
           <Button text="buy your ticket" />
@@ -24,12 +24,12 @@ export default function Hero() {
       <img
         alt="event images"
         src="images/hero-img1.png"
-        className="w-[30vw] absolute top-[-3rem] lg:top-[4rem] left-[-2rem] z-50 lg:z-[5] "
+        className="w-[30vw] lg:w-[20vw] absolute top-[-3rem] lg:top-[4rem] left-[-2rem] z-50 lg:z-[5] "
       />
       <img
         alt="event images"
         src="images/hero-img2.png"
-        className="w-[30vw] absolute bottom-[1rem] right-[-2rem] lg:bottom-[4rem] lg:right-[3rem] z-[5]"
+        className="w-[30vw] lg:w-[20vw] absolute bottom-[1rem] right-[-2rem] lg:bottom-[4rem] lg:right-[3rem] z-[5]"
       />
       {/* cloud images */}
       <img

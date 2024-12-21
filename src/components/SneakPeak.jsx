c32eaba848a74f411aec00ad43e030a4751e596f
artoftadashi <samuelolanrewaju05@gmail.com>
2024-12-21 13:05:54 +0000

    make sneak peak section responsive

--- src/components/SneakPeak.jsx
+++ src/components/SneakPeak.jsx
@@ -5,33 +5,43 @@ export default function SneakPeak() {
     {
       id: 1,
       image: "images/sneak-peak/img1.png",
-      style: " absolute w-[35vw] left-[8vw] top-[5vh]",
+      style:
+        "relative w-[70vw] lg:absolute lg:w-[35vw] lg:left-[8vw] lg:top-[5vh]",
+      divStyle: "w-full h-fit flex justify-start",
     },
     {
       id: 2,
       image: "images/sneak-peak/img2.png",
-      style: " absolute w-[35vw] right-[5vw] top-[20vh] ",
+      style:
+        " relative w-[70vw] lg:absolute w-[35vw] lg:right-[5vw] lg:top-[20vh] ",
+      divStyle: "w-full h-fit flex justify-end",
     },
     {
       id: 3,
       image: "images/sneak-peak/img3.png",
-      style: " absolute w-[35vw] left-[25vw]",
+      style:
+        " relative w-[70vw] lg:absolute lg:w-[35vw] lg:left-[25vw] rotate-[30deg] lg:rotate-0",
+      divStyle: "w-full h-fit flex justify-start",
     },
     {
       id: 4,
       image: "images/sneak-peak/img4.png",
-      style: " absolute w-[35vw] right-0 bottom-[20vh] ",
+      style:
+        " relative w-[70vw] lg:absolute lg:w-[35vw] lg:right-0 lg:bottom-[20vh] -rotate-[20deg] lg:rotate-0 ",
+      divStyle: "w-full h-fit flex justify-end",
     },
     {
       id: 5,
       image: "images/sneak-peak/img5.png",
-      style: " absolute w-[35vw] left-[5vw] bottom-[5vh] ",
+      style:
+        " relative w-[70vw] lg:absolute lg:w-[35vw] lg:left-[5vw] lg:bottom-[5vh] ",
+      divStyle: "w-full h-fit flex justify-start",
     },
   ];
   return (
     <section className="bg-[url('images/sneak-peak/BG.png')] bg-[#F7B5D2] bg-cover bg-no-repeat">
-      <div className="flex mx-auto justify-center items-center py-20">
-        <h1 className=" text-5xl font-semibold">
+      <div className="flex flex-col lg:flex-row mx-auto justify-center items-center py-20">
+        <h1 className=" text-3xl lg:text-5xl font-semibold text-center">
           Sneak peak of Last Year's Duos
         </h1>
         <img
@@ -40,9 +50,11 @@ export default function SneakPeak() {
           alt="peack sticker"
         />
       </div>
-      <div className="h-[250vh] relative flex justify-center items-center">
+      <div className="lg:h-[250vh] relative flex flex-col justify-center items-center py-12">
         {images.map((image) => (
-          <img className={image.style} src={image.image} key={image.id} />
+          <div className={image.divStyle}>
+            <img className={image.style} src={image.image} key={image.id} />
+          </div>
         ))}
       </div>
       <div className="pb-20 flex mx-auto justify-center">
